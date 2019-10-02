d8db884700f4c3de5a4ddae5c7a469d520e38a40
Marcio <alexandrino.marcio@gmail.com>
2019-10-02 15:20:54 -0300

    adding Cors and serving uploaded files

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
+const path = require('path');
 const routes = require('./routes');
 
 const app = express();
 
 mongoose.connect(process.env.MONGODB_KEY, { useNewUrlParser: true, useUnifiedTopology: true });
 
+app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
 app.listen(3333);
